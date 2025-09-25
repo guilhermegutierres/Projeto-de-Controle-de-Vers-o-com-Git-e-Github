ce5aafb2310fd2c9692465859741d720c79fa295
Guilherme Gutierres <135062828+guilhermegutierres@users.noreply.github.com>
2025-09-24 21:13:52 -0300

    Add files via upload

---
+++ jogos.py
@@ -0,0 +1,13 @@
+def menu():
+    while True:
+        print("\n=== MENU DE JOGOS ===")
+        print("Jogo 1")
+        print("Jogo 2")
+        print("Jogo 3")
+        print("Jogo 4")
+        print("Sair")
+        opcao = input("Escolha uma opção: ")
+
+# Iniciar o programa
+if __name__ == "__main__":
+    menu()
